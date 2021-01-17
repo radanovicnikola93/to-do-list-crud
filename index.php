c079d30254ed846a6547cd05376a0f6a4b47a06d
Nikola <radanovicnikola93@gmail.com>
2021-01-17 09:29:26 +0100

    displaying the READ property from the database in the Card format.

--- index.php
+++ index.php
@@ -53,6 +53,28 @@
         </form>
     </div>
 
+    <!--Obrazci za prikaz podatkov iz baze-->
+    <div class="container">
+        <div class="row m6">
+            <!--Vsebina iz baze-->
+            <?php
+            // vkljucimo template
+            // READ
+            include("./template/database.php");
+            while ($row = $result->fetch_assoc()): ?>
+                <div class="col-sm-4">
+                    <div class="card text-center mb-3 bg-success text-light">
+                        <div class="card-body">
+                            <h5 class="card-title text-uppercase fs-5 fw-bold"><?php echo $row['name']; ?></h5>
+                            <p class="card-text fw-lighter"><?php echo $row['date']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            <!--Zakljucimo while zanko-->
+            <?php endwhile; ?>
+        </div>
+    </div>
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   </body>
