aba6f3eeedd44d5a870f47619b1740be1a1e73bc
Nikola <radanovicnikola93@gmail.com>
2021-01-17 09:04:51 +0100

    added code to CREATEand insert data

--- form.php
+++ form.php
@@ -6,3 +6,26 @@ error_reporting(E_ALL);
 
 // povezemo PHP in MySQL z classom mysqli
 $mysqli = new mysqli('localhost', 'root', '', 'data');
+
+// v kolikor je napaka pri povezavi med php in bazo pokazi napako
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// CREATE
+if (isset($_POST['create'])) { // v kolikor je gumb pritisnjen nadaljuj 
+    // definiramo spremenljivke
+    $name = $_POST['name'];
+    $date = $_POST['date'];
+
+    // dodamo v bazo
+    $mysqli->query("INSERT INTO data (name, date) VALUES('$name', '$date')") or die($mysqli->error);
+
+    // session sporocila
+    $_SESSION['sporocilo'] = "Uspesno ste dodali opravilo!";
+    $_SESSION['tip_Sporocila'] = "success"; // success je ime Bootstrap classa
+
+    // redirect
+    header("location: index.php"); // ob ustvarjanju sporocila bo redirect ostal na isti strani
+}
