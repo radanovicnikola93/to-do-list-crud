aa02274204557a2baa63c26bb61ee28d075e9eeb
Nikola <radanovicnikola93@gmail.com>
2021-01-17 08:22:19 +0100

    created a simple form

--- index.php
+++ index.php
@@ -19,7 +19,31 @@
             </div>
         </nav>
     </div>
-    
+
+    <!--Form-->
+    <div class="container mb-6" id="formDiv">
+        <form action="" method="POST">
+            <div class="row g-2">
+                <!--Naslov-->
+                <div class="col-8">
+                    <div class="mb-6">
+                        <input type="text" name="name" class="form-control" value="" placeholder="Naslov">
+                    </div>
+                </div>
+                <!--Datum-->
+                <div class="col-2">
+                    <div class="mb-6">
+                        <input type="date" min="" id="date" name="date" class="form-control" value="" placeholder="Datum">
+                    </div>
+                </div>
+                <!--Button za posiljanje podatkov-->
+                <div class="col-2">
+                    <button type="submit" name="create" class="form-control btn btn-outline-success mb-6"></button>
+                </div>
+            </div>
+        </form>
+    </div>
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   </body>
